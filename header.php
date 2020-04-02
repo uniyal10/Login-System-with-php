3f7fb868616138fe7c7dadf82f39d5a2d9aa751d
sudhanshu uniyal <uniyal1001@gmail.com>
2020-04-03 02:37:55 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<header>
+		<nav>
+			<a href="#">
+				<img src="./img/logo.jjpg">
+			</a>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">About me</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			<div>
+				<form action="./includes/login.inc.php" method="post">
+					<input type="text" name="mailuid" placeholder="Username/E-mail...">
+					<input type="password" name="pwd" placeholder="Password...">
+					<button type="submit" name="login-submit">Login</button>
+				</form>
+				<a href="signup.php">Signup</a>
+				<form action="./includes/logout.inc.php" method="post">
+					<button type="submit" name="logout-submit">Logout</button>
+				</form>
+			</div>
+		</nav>
+	</header>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,12 @@
+<?php 
+
+  require "header.php";
+ ?>
+
+
+
+ <main>
+ 	
+      <p></p>
+
+ </main>
